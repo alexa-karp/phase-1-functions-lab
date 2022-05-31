b76bf205c45313b2d4b4b16b2a554b5ca3836cab
Alexa Karp <alexakarp@hotmail.com>
2022-05-31 01:07:08 -0500

    commit tree d7a844a4f0d599021b411b3da6bf3cb63de8777b

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+const hqStreet = 42
+const oneBlock = 264;
+
+function distanceFromHqInBlocks(streetNumber) {
+    if (hqStreet > streetNumber) {
+        return hqStreet - streetNumber;
+    }
+
+    else {
+        return streetNumber - hqStreet;
+    }
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber) * oneBlock;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination){
+        return (start - destination) * oneBlock;
+    }
+
+    else {
+        return (destination - start) * oneBlock;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+    }
+
+    if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+     }
+
+     if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+     }
+
+     else {
+         return 'cannot travel that far';
+     }
+   
+}
